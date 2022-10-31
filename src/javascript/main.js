32f9eb6a916ecd1abd9b191394dbe4ef5d1e6d19
Ivan Reis <109477902+Ivan-ReisDev@users.noreply.github.com>
2022-10-31 10:39:33 -0300

    Update main.js

--- src/javascript/main.js
+++ src/javascript/main.js
@@ -1,99 +1,94 @@
-var variavelMulta = 0;
+var MultaPorcentagemValor = 0;
 var valoBoleto = 0;
 var dataP = '0';
 var dataV = '0';
 var days = 0;
-var jurosTotal = 0;
 
-  function Vencimento(){
+
+function Vencimento(){
     
     dataV = document.querySelector('input#vencimento').value;
-    
     dataV = new Date(dataV );
-
-    console.log('Vencimento', dataV.toLocaleDateString('pt-br'));
-
   }
   
-  function DataPagamento(){
+function DataPagamento(){
     
     dataP = document.querySelector('input#pagamento').value;
     dataP =  new Date(dataP);
-
-    console.log('Data de Pagamento: ', dataP.toLocaleDateString('pt-br'));
-    
-  }
+}
   
-
-const Multa = () =>{
-  var select = document.getElementById('lista-multa');
-  
-  select.onchange = function(){
-    variavelMulta = this.value;
+function Multa(){
+    var select = document.getElementById('lista-multa');
+    select.onchange = function(){
+    MultaPorcentagemValor = this.value;
     }
 }
 
 function ValorDoBoleto(){
   let valor = document.querySelector("#valor-boleto").value;
   valoBoleto = parseInt(valor);
+
 }
 
+
 function calcularDias(){
-    //juros
-    const jrs = document.getElementById('juros')
+   //juros
+   const jrs = document.getElementById('juros')
      
    const diasAtrasados = document.getElementById('diasAtras')
-
+ 
    //diferença de dias
    let total = (dataP - dataV);
    let totalT = 1000 * 60 * 60 * 24;
-    days = total / totalT;
+   days = total / totalT;
 
+     //multa
+     const valorM = document.getElementById('valorMulta');
+
+     //valorBoleto
+     const valor2 = document.getElementById('valor');
+   
+     //valo total de multa
+     let valorTotalMulta = MultaPorcentagemValor * valoBoleto;
+     valorM.textContent = valorTotalMulta;
+   
+     //valor total boleto
+     var somaMultaBoleto = (valorTotalMulta + valoBoleto);
+     let jurosTotal = (somaMultaBoleto * 1/100/30 * days)
+     let JurosEmReal = jurosTotal * somaMultaBoleto / 100;
+     let total1 = somaMultaBoleto + JurosEmReal;
+     valor2.textContent = total1.toFixed(2);  
+    
 
    if(days <= 0){
-     diasAtrasados.textContent = 0;
-    jurosTotal = (valoBoleto * 1/100/30 * 0)
+    diasAtrasados.textContent = 0;
+    let jurosTotal = (somaMultaBoleto * 1/100/30 * 0)
     jrs.textContent = jurosTotal.toFixed(2);
-
    }
    else{
-     diasAtrasados.textContent = Math.abs(days);
-    jurosTotal = (valoBoleto * 1/100/30 * days)
-    jrs.textContent = jurosTotal.toFixed(2);
+    diasAtrasados.textContent = Math.abs(days);
+    let jurosTotal = (somaMultaBoleto * 1/100/30 * days)
+    let JurosEmReal = jurosTotal * somaMultaBoleto / 100;
+    jrs.textContent = JurosEmReal.toFixed(2);
+    console.log(JurosEmReal.toFixed(2));
    }
- 
-}
 
-function CalcularFatura(){
-    //multa
-    const valorM = document.getElementById('valorMulta')
-
-    //valorBoleto
-    const valor2 = document.getElementById('valor')
-
-  
-    //valo total de multa
-    let valorTotalMulta = variavelMulta * valoBoleto;
-    valorM.textContent = valorTotalMulta;
-  
-    //valor total boleto
-    
-    var soma = (valorTotalMulta + valoBoleto);
-    let addJuros = jurosTotal * soma / 100
-    let total = soma + addJuros;
-    valor2.textContent = total.toFixed(2);  
 
 }
+
+
+
+
 
 
 const calcular = () =>{
   calcularDias();
-  CalcularFatura();
-
+  Multa();
 }
 
 
-Multa();
+
+
 
 
 

6a29c225565ba1be288e5bba6cc279bdf0ec7fa0
Ivan Reis <109477902+Ivan-ReisDev@users.noreply.github.com>
2022-12-06 14:58:21 -0300

    Update main.js

--- src/javascript/main.js
+++ src/javascript/main.js
@@ -1,109 +1,125 @@
-var MultaPorcentagemValor = 0;
-var valoBoleto = 0;
-var dataP = '0';
-var dataV = '0';
-var days = 0;
+//formulario
+const $boxPrincipal = document.getElementById('box-principal');
 
-function LimpaInput(){
-  const campoVencimento = document.querySelector('#vencimento');
-  const campoPagamento = document.querySelector('#pagamento')
-  const campoLista = document.querySelector('#lista-multa')
-  const campoBoleto = document.querySelector('#valor-boleto')
-  campoVencimento.value = '';
-  campoPagamento.value = '';
-  campoLista.value = '';
-  campoBoleto.value = '';
-}
+//inputs
+const $vencimento = document.getElementById('vencimento');
+const $valorBoleto = document.getElementById('valor-boleto');
+const $pagamento = document.getElementById('pagamento');
+const $listaMulta = document.getElementById('lista-multa');
 
-function Vencimento(){
-    
-    dataV = document.querySelector('input#vencimento').value;
-    dataV = new Date(dataV );
+//botoes
+const $calcular = document.getElementById('calcular');
+const $limpar = document.getElementById('limpar');
+
+//tabela
+const $diasAtrasados = document.getElementById('diasAtras');
+const $valorMulta = document.getElementById('valorMulta');
+const $juros = document.getElementById('juros');
+const $valor = document.getElementById('valor');
+
+//dias 
+const second = 1000;
+const minute = second * 60;
+const hour = minute * 60;
+const day = hour * 24;
+
+//Evento de calcular com o enter
+$boxPrincipal.addEventListener('keyup', (e) => {
+  if (e.keyCode == 13) {
+    calculoGeral();
   }
-  
-function DataPagamento(){
-    
-    dataP = document.querySelector('input#pagamento').value;
-    dataP =  new Date(dataP);
-}
-  
-function Multa(){
-    let Multa = document.querySelector('input#lista-multa').value;
-    MultaPorcentagemValor = Number(Multa) / 100;
+});
 
-}
+// Limpar input e tabela apertando a letra L;
+$boxPrincipal.addEventListener('keyup', (e) => {
+  if (e.keyCode == 46) {
+    cleanInput()
+    cleanTabela()
+  }
+});
 
-function ValorDoBoleto(){
-  let valor = document.querySelector("#valor-boleto").value;
-  valoBoleto = Number(valor);
+//Evento de calcular com o click
+$calcular.addEventListener('click', (e) => {
+  calculoGeral();
+});
 
-}
+//Limpar inputs e tabelas clicando no botão limpar
+$limpar.addEventListener('click', (e) => {
+  cleanInput()
+  cleanTabela()
+})
 
+// Mascara do input valor do boleto (aparere na tela do usuário)
+$valorBoleto.addEventListener("keyup", formatarMoeda);
+function formatarMoeda(e) {
+  let v = e.target.value.replace(/\D/g, "");
+  v = (v / 100).toFixed(2) + "";
+  v = v.replace(".", ",");
+  v = v.replace(/(\d)(\d{3})(\d{3}),/g, "$1.$2.$3,");
+  v = v.replace(/(\d)(\d{3}),/g, "$1.$2,");
+  e.target.value = v;
+  $valorBoleto = v
 
-function calcularDias(){
-   //juros
-   const jrs = document.getElementById('juros')
-     
-   const diasAtrasados = document.getElementById('diasAtras')
- 
-   //diferença de dias
-   let total = (dataP - dataV);
-   let totalT = 1000 * 60 * 60 * 24;
-   days = total / totalT;
+};
 
-     //multa
-     const valorM = document.getElementById('valorMulta');
+//Função limpa input
+function cleanInput() {
+  $vencimento.value = '';
+  $pagamento.value = '';
+  $listaMulta.value = '';
+  $valorBoleto.value = '';
+};
 
-     //valorBoleto
-     const valor2 = document.getElementById('valor');
-   
-     //valo total de multa
-     let valorTotalMulta = MultaPorcentagemValor * valoBoleto;
-     valorM.textContent = valorTotalMulta.toFixed(2);
-     valorM.textContent = valorTotalMulta.toLocaleString('pt-BR', {style: 'currency', currency:'BRL'})
+//Função limpa tabela
+function cleanTabela() {
+  $diasAtrasados.innerHTML = '0';
+  $valorMulta.innerHTML = 'R$ 0,00';
+  $juros.innerHTML = 'R$ 0,00';
+  $valor.innerHTML = 'R$ 0,00';
+};
 
-     //valor total boleto
-     var somaMultaBoleto = (valorTotalMulta + valoBoleto);
-     let jurosTotal = (100 * 1/100/30 * days)
-     let JurosEmReal = jurosTotal * valoBoleto / 100;
-     let total1 = somaMultaBoleto + JurosEmReal;
-     valor2.textContent = total1.toFixed(2);  
-     valor2.textContent = total1.toLocaleString('pt-BR', {style: 'currency', currency:'BRL'})
-    
-
-   if(days <= 0){
-    diasAtrasados.textContent = 0;
-    let jurosTotal = (100 * 1/100/30 * 0)
-    jrs.textContent = jurosTotal.toFixed(2);
-    jrs.textContent = jurosTotal.toLocaleString('pt-BR', {style: 'currency', currency:'BRL'})
-   }
-   else{
-    diasAtrasados.textContent = Math.abs(days);
-    let jurosTotal = (100 * 1/100/30 * days)
-    let JurosEmReal = valoBoleto * jurosTotal / 100;
-    jrs.textContent = JurosEmReal.toFixed(2);
-    jrs.textContent = JurosEmReal.toLocaleString('pt-BR', {style: 'currency', currency:'BRL'})
-    console.log('Total Juros', jurosTotal);
- 
-   }
-
-
+function ContadorDias() {
+  let date_ini = new Date(document.getElementById('pagamento').value);
+  let date_end = new Date(document.getElementById('vencimento').value);
+  let diff = date_ini.getTime() - date_end.getTime();
+  let res = Math.floor(diff / day);
+  return res
 }
 
+function jurosCalculo() {
+  //ValorBoleto x 1/100/30*dias
 
-
-
-
-
-const calcular = () =>{
-  calcularDias();
-  Multa();
+  let dias = ContadorDias()
+  let mes = 30;
+  let ResJuros = transBoleto() * 1 / 100 / mes * dias;
+  return ResJuros;
 }
 
+function calculoMulta() {
+  //ValorBoleto * Porcentagem/100
+  let PorcentagemJuros = $listaMulta.value
+  PorcentagemJuros = PorcentagemJuros / 100;
+  return Number(PorcentagemJuros * transBoleto());
+}
 
+function transBoleto() {
+  let TransBoleto = $valorBoleto.value;
+  let transform = TransBoleto.replace(/\./g, "");
+  transform = transform.replace(/,/g, ".");
+  return Number(transform)
+}
 
+function calculoGeral() {
+  let ResValorTotal = calculoMulta() + jurosCalculo() + transBoleto();
+  //Alterações no DOM
+  $diasAtrasados.textContent = ContadorDias();
 
+  $valorMulta.textContent = calculoMulta().toFixed(2);
+  $valorMulta.textContent = calculoMulta().toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })
 
+  $juros.textContent = jurosCalculo().toFixed(2);
+  $juros.textContent = jurosCalculo().toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })
 
-
-
+  $valor.textContent = ResValorTotal.toFixed(2);
+  $valor.textContent = ResValorTotal.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })
+}
